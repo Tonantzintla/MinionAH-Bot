0810d4f23c211b1732ac035cff4de12e45d288de
Nikos Andriotis <67287051+andriotisnikos1@users.noreply.github.com>
2024-09-09 18:29:49 +0300

    file based commands

---
+++ src/commands/price-check/prices.ts
@@ -0,0 +1,5 @@
+import { SlashCommandBuilder } from "discord.js";
+
+export default new SlashCommandBuilder()
+        .setName("prices")
+        .setDescription("Get the price for a minion. Or all of them!")
--- src/discord/applySlashCommands.ts
+++ src/discord/applySlashCommands.ts
@@ -1,14 +1,9 @@
-import { REST, Routes, SlashCommandBuilder } from "discord.js";
-import { client } from "../../discord/client.js";
-
-const commands = [
-    new SlashCommandBuilder()
-        .setName("prices")
-        .setDescription("Get the price")
-].map(command => command.toJSON());
+import { REST, Routes } from "discord.js";
+import { client } from "./client.js";
+import commands from "../commands/commands.js";
 
 export default async function applySlashCommands() {
-    const rest = new REST({ version: '9' }).setToken(process.env.TOKEN!);
+    const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN!);
     try {
         for (const guild of client.guilds.cache.values()) {
             await rest.put(
