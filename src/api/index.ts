bf104e9f94f5b482253a4f714bc4e75c7be5041d
RioTheDev <dateshidze.gio@gmail.com>
2024-09-09 21:12:22 +0400

    feat: add notifications route to the app

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import notifications from "./notification/index.js";
 
+// Create app
 const app = express();
 
-async function init(): Promise<void> {
-  app.use(express.json());
+//Middlewares
+app.use(express.json());
 
+//Routes
+app.use("/notifications", notifications);
+
+async function init(): Promise<void> {
   return new Promise((res) => {
     app.listen(process.env.WEB_PORT!, () => {
       console.log(`Web api running on port: ${process.env.WEB_PORT!}`);
