00a95f856467f354ea9aea9eef4736af3526136a
Nikos Andriotis <67287051+andriotisnikos1@users.noreply.github.com>
2024-09-09 18:13:31 +0300

    c

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import "./central.config.js"
 import { client } from "./discord/client.js"
+import applySlashCommands from "./lib/global/applySlashCommands.js";
 
-client.on("ready", (bot) => {
+client.on("ready", async (bot) => {
     console.log(`Bot ready! Process ID: ${process.pid}`);
-    // await applySlashCommands(client);
+    await applySlashCommands();
 })
 
 client.login(process.env.DISCORD_TOKEN);
