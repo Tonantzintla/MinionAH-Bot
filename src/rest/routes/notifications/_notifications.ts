695faad2850104b346c51ca070acb9a136624c00
Nikos Andriotis <67287051+andriotisnikos1@users.noreply.github.com>
2025-05-10 03:13:33 +0300

    feat: add test notification sending capabilities

--- src/rest/routes/notifications/_notifications.ts
+++ src/rest/routes/notifications/_notifications.ts
@@ -2,9 +2,12 @@ import zodMW from "$rest/middleware/zodMW.js";
 import notif_send_schemaZod from "$rest/zod/notifications/notif_send_schema.zod.js";
 import e from "express";
 import send from "./send.js";
+import test from "./test.js";
+import notif_test_schemaZod from "$src/rest/zod/notifications/notif_test_schema.zod.js";
 
 const notifications = e.Router();
 
 notifications.post("/send", zodMW(notif_send_schemaZod),send);
+notifications.post("/test", zodMW(notif_test_schemaZod), test)
 
 export default notifications;
---
+++ src/rest/zod/notifications/notif_test_schema.zod.ts
@@ -0,0 +1,12 @@
+import zod from "zod";
+export default zod.object({
+  receiverDiscordID: zod
+    .string()
+    .regex(/^\d{18}$/, "Discord ID must be an 18 digit string."),
+  senderID: zod
+    .string()
+    .length(32, { message: "UUID must be exactly 32 characters long" })
+    .regex(/^[a-fA-F0-9]+$/, {
+      message: "UUID must contain only hexadecimal characters (0-9, a-f, A-F)",
+    }),
+});
