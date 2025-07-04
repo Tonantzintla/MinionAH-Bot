0949e865b7c136a4a2fdfca7a64ab4ea2fef0d01
Nikos Andriotis <67287051+andriotisnikos1@users.noreply.github.com>
2025-07-04 20:38:33 +0300

    fix test: remove full refresh of commands. now, they are just resupplied from the bot to discord

--- src/discord/applySlashCommands.ts
+++ src/discord/applySlashCommands.ts
@@ -1,21 +1,20 @@
 import commands from "$src/commands/commands";
 import { REST, Routes } from "discord.js";
-import { client } from "./client";
 
 export default async function applySlashCommands() {
   const rest = new REST({ version: "9" }).setToken(process.env.DISCORD_TOKEN!);
   try {
     // delete all commands from all guilds - migration to global commands
-    for (const guild of client.guilds.cache.values()) {
-      await rest.put(
-        Routes.applicationGuildCommands(process.env.APP_ID!, guild.id),
-        { body: [] }
-      );
-    }
+    // for (const guild of client.guilds.cache.values()) {
+    //   await rest.put(
+    //     Routes.applicationGuildCommands(process.env.APP_ID!, guild.id),
+    //     { body: [] }
+    //   );
+    // }
     // delete all global commands
-    await rest.put(Routes.applicationCommands(process.env.APP_ID!), {
-      body: []
-    });
+    // await rest.put(Routes.applicationCommands(process.env.APP_ID!), {
+    //   body: []
+    // });
     // register new global commands
     await rest.put(Routes.applicationCommands(process.env.APP_ID!), {
       body: commands
