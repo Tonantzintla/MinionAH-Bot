7328c4f0414e704edc423b28ce1c4e4ae2a5458b
Nikos Andriotis <67287051+andriotisnikos1@users.noreply.github.com>
2025-10-03 16:15:19 +0300

    c

---
+++ src/commands/auctions/search/util/SearchSequence.ts
@@ -0,0 +1,90 @@
+import constructAuctionPageDisplayContainer from "./constructAuctionPageDisplayContainer";
+import getAuctionData from "./getAuctionData";
+
+interface SequenceConstructorParams {
+    minionType?: string;
+    minionTier?: number;
+    overrideDefaultAuctionGetter?: typeof getAuctionData
+}
+
+interface SequenceGetParams extends SequenceConstructorParams {
+    username: string,
+    explicitlyCreate?: boolean
+
+}
+
+const searchConfig = {
+    auctionsPerPage: 5,
+}
+
+export default class SearchSequence {
+    private static instances: Map<string, SearchSequence> = new Map();
+    private minionType?: string;
+    private minionTier?: number;
+    public currentPageNumber: number = 1;
+    private sortingOrder: "asc" | "desc" | undefined = "desc"
+    private auctionGetter: typeof getAuctionData = getAuctionData;
+
+    private constructor({ minionType, minionTier, overrideDefaultAuctionGetter }: SequenceConstructorParams) {
+        this.minionType = minionType;
+        this.minionTier = minionTier;
+        if (overrideDefaultAuctionGetter) this.auctionGetter = overrideDefaultAuctionGetter;
+    }
+
+    public static getSequence({ username, minionType, minionTier, explicitlyCreate = false, overrideDefaultAuctionGetter }: SequenceGetParams) {
+        const existingInstance = this.instances.get(username);
+        if (existingInstance && !explicitlyCreate) {
+            return existingInstance;
+        } else {
+            const newInstance = new SearchSequence({ minionType, minionTier, overrideDefaultAuctionGetter });
+            this.instances.set(username, newInstance);
+            return newInstance;
+        }
+    }
+
+    public changeSortingOrder(newOrder: "asc" | "desc" | "none") {
+        if (newOrder === "none") {
+            this.sortingOrder = undefined;
+        } else {
+            this.sortingOrder = newOrder;
+        }
+    }
+
+    public async getCurrentPage() {
+        this.currentPageNumber--;
+        return this.nextPage();
+    }
+
+    public navigateToPage(page: number) {
+        if (isNaN(page)) page = 0;
+        this.currentPageNumber = Math.max(0, page - 1);
+        return this.nextPage();
+    }
+
+    public async previousPage() {
+        this.currentPageNumber = Math.max(0, this.currentPageNumber - 2);
+        return this.nextPage();
+    }
+
+    public async nextPage() {
+        this.currentPageNumber += 1;
+
+        const auctionData = await this.auctionGetter({
+            auctionsPerPage: searchConfig.auctionsPerPage,
+            page: this.currentPageNumber,
+            minionType: this.minionType,
+            minionTier: this.minionTier,
+            sortingOrder: this.sortingOrder
+        })
+
+        return {
+            auctionData,
+            visualContainer: await constructAuctionPageDisplayContainer({
+                ...auctionData,
+                page: this.currentPageNumber,
+                pageSize: searchConfig.auctionsPerPage,
+                currentSortingOrder: this.sortingOrder
+            })
+        }
+    }
+}
